c549f9f9dd9be1d53f4182454d1e9533b82fc3b0
roman454647 <37308167+roman454647@users.noreply.github.com>
2019-05-02 19:17:59 +0300

    bitkoin

--- web_dev/Project4/js/web_storage.js
+++
@@ -1,9 +0,0 @@
-function counter() {
-   if(localStorage.hits){
-  localStorage.hits=Number(localStorage.hits)+1;
- }
-else{
-localStorage.hits=1;
-}
-document.getElementById('output').innerHTML= "You have clicked counter button for"+ " "+ localStorage.hits +" "+"times";
-}
