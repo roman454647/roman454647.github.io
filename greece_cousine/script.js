1d253f5bb30599d52c877c4b7ac86d2c76f57919
roman454647 <37308167+roman454647@users.noreply.github.com>
2019-08-04 18:01:46 +0300

    greece.cousine

---
+++ greece_cousine/script.js
@@ -0,0 +1,35 @@
+//function to show ,hide nav bar
+navbar.onclick = function myFunction(){
+  var x = document.getElementById('navigation1');
+
+  if(x.className ==="navigation"){
+    x.className+=" responsive";
+  } else{
+    x.className = 'navigation'
+  }
+}
+
+var myIndex = 0;
+carousel();
+//function for slider (auto)
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("rewiew");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel, 9000);
+}
+//animation for transition fro one part of page to another
+
+$(document).ready(function(){
+    $("#navig").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('index,html').animate({scrollTop: top}, 3000);
+    });
+});
---
+++ java-script/dom/code.js
@@ -0,0 +1,8 @@
+var hider = document.querySelector('#hider')
+var text = document.getElementById('text')
+console.log(hider ,text)
+
+hider.addEventListener('click',function(){
+  console.log("blabla");
+  hider.style.display ='none'
+})
